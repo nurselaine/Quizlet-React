2ed5a8b6461a3bff0cd1239eaa6fe5a8f9c53637
nurselaine <Elaine.t.huynh@gmail.com>
2022-06-09 13:28:31 -0700

    imported footer to app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
-import logo from '../logo.svg';
+// import logo from '../logo.svg';
 import '../css/App.css';
-import Navbar from './Navbar';
+import AppNavbar from './Navbar';
 import HomeSpash from './HomeSpash';
 import Footer from './Footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-        <Navbar/>
+        <AppNavbar/>
         <HomeSpash/>
         <Footer/>
     </div>
